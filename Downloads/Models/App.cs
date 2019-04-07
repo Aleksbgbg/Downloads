77cfea0baf56d899414569c84a1a44832888addd
Aleksbgbg <kozichka01@abv.bg>
2019-04-07 18:43:59 +0100

    [App] Add app model which represents an application to download.

---
+++ Downloads/Models/App.cs
@@ -0,0 +1,26 @@
+﻿namespace Downloads.Models
+{
+    using System;
+
+    public class App
+    {
+        public App(string name, string latestVersion, string description, DateTime lastUpdated, string gitHubUrl)
+        {
+            Name = name;
+            LatestVersion = latestVersion;
+            Description = description;
+            LastUpdated = lastUpdated;
+            GitHubUrl = gitHubUrl;
+        }
+
+        public string Name { get; }
+
+        public string LatestVersion { get; }
+
+        public string Description { get; }
+
+        public DateTime LastUpdated { get; }
+
+        public string GitHubUrl { get; }
+    }
+}
