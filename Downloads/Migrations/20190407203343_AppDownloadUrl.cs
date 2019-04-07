55d963dd10784651d74d79d96f1f71d02baacb1d
Aleksbgbg <kozichka01@abv.bg>
2019-04-07 21:34:50 +0100

    [Migrations] Add migrations for download URL in app model.

---
+++ Downloads/Migrations/20190407203343_AppDownloadUrl.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Downloads.Migrations
+{
+    public partial class AppDownloadUrl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DownloadUrl",
+                table: "Apps",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DownloadUrl",
+                table: "Apps");
+        }
+    }
+}
