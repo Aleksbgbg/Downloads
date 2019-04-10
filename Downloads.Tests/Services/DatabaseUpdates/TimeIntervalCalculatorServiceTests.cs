54bd3f6b7f381374ddecefbad2a3464cc2d40100
Aleksbgbg <kozichka01@abv.bg>
2019-04-10 10:16:32 +0100

    [TimeIntervalCalculator] Add logic to calculate 24-hour time differences, by roudning to the next day.

--- Downloads.Tests/Services/DatabaseUpdates/TimeIntervalCalculatorServiceTests.cs
+++ Downloads.Tests/Services/DatabaseUpdates/TimeIntervalCalculatorServiceTests.cs
@@ -11,18 +11,53 @@
         [Fact]
         public void TestCalculateTimeUntilDeadline()
         {
-            DateTime now = new DateTime(2020, 6, 15, 18, 00, 00);
-            DateTime deadline = new DateTime(now.Year, now.Month, now.Day, 22, 00, 00);
-            TimeSpan timeUntilDeadline = deadline - now;
+            TwentyFourHourTimeDifference twentyFourHourTimeDifference = TwentyFourHourTimeDifference.FromTo(18, 22);
+            TimeSpan timeUntilDeadline = TimeSpan.FromHours(4);
 
-            TimeIntervalCalculatorService timeIntervalCalculatorService = new TimeIntervalCalculatorService
-            {
-                SourceTime = now
-            };
-
-            TimeSpan result = timeIntervalCalculatorService.CalculateTimeUntilDeadline(deadline);
+            TimeSpan result = twentyFourHourTimeDifference.CalculateTimeUntilHourNextReached();
 
             Assert.Equal(timeUntilDeadline, result);
+        }
+
+        [Fact]
+        public void TestCalculateTimeUntilDeadlineWhenDeadlineBeforeCurrentTime()
+        {
+            TwentyFourHourTimeDifference twentyFourHourTimeDifference = TwentyFourHourTimeDifference.FromTo(23, 22);
+            TimeSpan timeUntilDeadline = TimeSpan.FromHours(23);
+
+            TimeSpan result = twentyFourHourTimeDifference.CalculateTimeUntilHourNextReached();
+
+            Assert.Equal(timeUntilDeadline, result);
+        }
+
+        private class TwentyFourHourTimeDifference
+        {
+            private readonly DateTime _startingTime;
+
+            private readonly DateTime _deadline;
+
+            private TwentyFourHourTimeDifference(int currentHour, int deadlineHour)
+            {
+                _startingTime = new DateTime(1, 1, 1, currentHour, 00, 00);
+                _deadline = new DateTime(2020, 6, 15, deadlineHour, 00, 00);
+            }
+
+            public static TwentyFourHourTimeDifference FromTo(int currentHour, int deadlineHour)
+            {
+                return new TwentyFourHourTimeDifference(currentHour, deadlineHour);
+            }
+
+            public TimeSpan CalculateTimeUntilHourNextReached()
+            {
+                TimeIntervalCalculatorService timeIntervalCalculatorService = new TimeIntervalCalculatorService
+                {
+                    SourceTime = _startingTime
+                };
+
+                TimeSpan result = timeIntervalCalculatorService.CalculateTimeUntilDeadline(_deadline);
+
+                return result;
+            }
         }
     }
 }
--- Downloads/Services/DatabaseUpdates/TimeIntervalCalculatorService.cs
+++ Downloads/Services/DatabaseUpdates/TimeIntervalCalculatorService.cs
@@ -8,7 +8,17 @@
 
         public TimeSpan CalculateTimeUntilDeadline(DateTime deadline)
         {
+            deadline = CoerceDeadlineFollowingSourceTime(deadline);
             return deadline - SourceTime;
+        }
+
+        private DateTime CoerceDeadlineFollowingSourceTime(DateTime deadline)
+        {
+            TimeSpan sourceTimeDifference = SourceTime - deadline;
+            double totalDayDifference = sourceTimeDifference.TotalDays;
+            double differenceInFullDays = Math.Ceiling(totalDayDifference);
+
+            return deadline.AddDays(differenceInFullDays);
         }
     }
 }
