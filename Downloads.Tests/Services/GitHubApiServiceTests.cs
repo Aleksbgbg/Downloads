4e10e368323b0388ab25ea86b3ae8b0189ab4f94
Aleksbgbg <kozichka01@abv.bg>
2019-04-28 15:15:27 +0100

    [GitHubApiService] Extract responsibilities from service to depend on other services.

--- Downloads.Tests/Services/GitHubApiServiceTests.cs
+++ Downloads.Tests/Services/GitHubApiServiceTests.cs
@@ -1,21 +1,150 @@
 ﻿namespace Downloads.Tests.Services
 {
-    using Downloads.Services;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
 
-    using global::Octokit;
+    using Downloads.Models;
+    using Downloads.Services;
+    using Downloads.Services.GitHub;
+    using Downloads.Tests.Api;
 
     using Moq;
+    using Moq.Language;
+
+    using Xunit;
 
     public class GitHubApiServiceTests
     {
-        private readonly Mock<IGitHubClient> _gitHubClientMock;
+        private readonly Mock<IRepositoryFinderService> _repositoryFinderServiceMock;
+
+        private readonly Mock<IReleaseFinderService> _releaseFinderServiceMock;
+
+        private readonly Mock<IRepositoryToAppGeneratorService> _repositoryToAppGeneratorServiceMock;
 
         private readonly GitHubApiService _gitHubApiService;
 
         public GitHubApiServiceTests()
         {
-            _gitHubClientMock = new Mock<IGitHubClient>();
-            _gitHubApiService = new GitHubApiService(_gitHubClientMock.Object);
+            _repositoryFinderServiceMock = new Mock<IRepositoryFinderService>();
+
+            _releaseFinderServiceMock = new Mock<IReleaseFinderService>();
+
+            _repositoryToAppGeneratorServiceMock = new Mock<IRepositoryToAppGeneratorService>();
+
+            _gitHubApiService = new GitHubApiService(_repositoryFinderServiceMock.Object, _releaseFinderServiceMock.Object, _repositoryToAppGeneratorServiceMock.Object);
+        }
+
+        [Fact]
+        public async Task TestGetsAllRepositories()
+        {
+            IEnumerable<App> apps = await _gitHubApiService.GetReleasedGitHubApps();
+
+            VerifyGetAllRepositoriesCalled();
+        }
+
+        [Fact]
+        public async Task TestChecksRepositoriesHaveReleases()
+        {
+            IGitHubRepository[] repositories = SetupRepositories(2);
+
+            IEnumerable<App> apps = await _gitHubApiService.GetReleasedGitHubApps();
+
+            VerifyHasReleasesCalled(repositories[0]);
+            VerifyHasReleasesCalled(repositories[1]);
+        }
+
+        [Fact]
+        public async Task TestGetsLatestReleaseForRepositoriesWithReleases()
+        {
+            IGitHubRepository[] repositories = SetupRepositories(new IGitHubRepository[]
+            {
+                SetupRepository(hasReleases: true),
+                SetupRepository(hasReleases: false),
+                SetupRepository(hasReleases: true)
+            });
+
+            IEnumerable<App> apps = await _gitHubApiService.GetReleasedGitHubApps();
+
+            VerifyGetLatestReleaseCalled(repositories[0]);
+            VerifyGetLatestReleaseCalled(repositories[1], Times.Never);
+            VerifyGetLatestReleaseCalled(repositories[2]);
+        }
+
+        [Fact]
+        public async Task TestMapsAppsCorrectly()
+        {
+            App[] generatedApps = SetupApps();
+            App[] releasedApps = (await _gitHubApiService.GetReleasedGitHubApps()).ToArray();
+
+            Assert.Equal(generatedApps, releasedApps);
+        }
+
+        private App[] SetupApps()
+        {
+            App[] apps = Data.Apps;
+            IGitHubRepository[] repositories = new IGitHubRepository[apps.Length];
+
+            ISetupSequentialResult<App> sequenceSetup = _repositoryToAppGeneratorServiceMock.SetupSequence(appGenerator => appGenerator.GenerateApp(It.IsAny<IGitHubRepositoryDataProvider>()));
+
+            for (int appIndex = 0; appIndex < apps.Length; appIndex++)
+            {
+                repositories[appIndex] = SetupRepository();
+                sequenceSetup.Returns(apps[appIndex]);
+            }
+
+            SetupRepositories(repositories);
+
+            return apps;
+        }
+
+        private IGitHubRepository[] SetupRepositories(int quantity)
+        {
+            IGitHubRepository[] repositories = new IGitHubRepository[quantity];
+
+            for (int repositoryIndex = 0; repositoryIndex < quantity; repositoryIndex++)
+            {
+                repositories[repositoryIndex] = SetupRepository();
+            }
+
+            return SetupRepositories(repositories);
+        }
+
+        private IGitHubRepository[] SetupRepositories(IGitHubRepository[] repositories)
+        {
+            _repositoryFinderServiceMock.Setup(repositoryFinder => repositoryFinder.GetAllRepositories()).ReturnsAsync(repositories);
+
+            return repositories;
+        }
+
+        private IGitHubRepository SetupRepository(bool hasReleases = true)
+        {
+            Mock<IGitHubRepository> gitHubRepositoryMock = new Mock<IGitHubRepository>();
+
+            _releaseFinderServiceMock.Setup(releaseFinder => releaseFinder.HasReleases(gitHubRepositoryMock.Object)).ReturnsAsync(hasReleases);
+
+            return gitHubRepositoryMock.Object;
+        }
+
+        private void VerifyGetAllRepositoriesCalled()
+        {
+            _repositoryFinderServiceMock.Verify(repositoryFinder => repositoryFinder.GetAllRepositories());
+        }
+
+        private void VerifyHasReleasesCalled(IGitHubRepository repository)
+        {
+            _releaseFinderServiceMock.Verify(releaseFinder => releaseFinder.HasReleases(repository));
+        }
+
+        private void VerifyGetLatestReleaseCalled(IGitHubRepository repository)
+        {
+            VerifyGetLatestReleaseCalled(repository, Times.Once);
+        }
+
+        private void VerifyGetLatestReleaseCalled(IGitHubRepository repository, Func<Times> times)
+        {
+            _releaseFinderServiceMock.Verify(releaseFinder => releaseFinder.GetLatestRelease(repository), times);
         }
     }
 }
--- Downloads/Services/GitHubApiService.cs
+++ Downloads/Services/GitHubApiService.cs
@@ -1,95 +1,50 @@
 ﻿namespace Downloads.Services
 {
-    using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
 
     using Downloads.Models;
-
-    using Octokit;
+    using Downloads.Services.GitHub;
 
     public class GitHubApiService : IGitHubApiService
     {
-        private readonly IGitHubClient _gitHubClient;
+        private readonly IRepositoryFinderService _repositoryFinderService;
 
-        public GitHubApiService(IGitHubClient gitHubClient)
+        private readonly IReleaseFinderService _releaseFinderService;
+
+        private readonly IRepositoryToAppGeneratorService _repositoryToAppGeneratorService;
+
+        public GitHubApiService(IRepositoryFinderService repositoryFinderService, IReleaseFinderService releaseFinderService, IRepositoryToAppGeneratorService repositoryToAppGeneratorService)
         {
-            _gitHubClient = gitHubClient;
+            _repositoryFinderService = repositoryFinderService;
+            _releaseFinderService = releaseFinderService;
+            _repositoryToAppGeneratorService = repositoryToAppGeneratorService;
         }
 
         public async Task<IEnumerable<App>> GetReleasedGitHubApps()
         {
-            IReadOnlyList<Repository> repositories = await _gitHubClient.Repository.GetAllForUser("Aleksbgbg");
+            IGitHubRepository[] repositories = (await _repositoryFinderService.GetAllRepositories()).ToArray();
 
-            List<App> apps = new List<App>();
+            App[] apps = new App[repositories.Length];
 
-            for (int repositoryIndex = 0; repositoryIndex < repositories.Count; ++repositoryIndex)
+            for (int repositoryIndex = 0; repositoryIndex < repositories.Length; repositoryIndex++)
             {
-                Repository repository = repositories[repositoryIndex];
+                IGitHubRepository repository = repositories[repositoryIndex];
 
-                try
+                bool hasReleases = await _releaseFinderService.HasReleases(repository);
+
+                if (hasReleases)
                 {
-                    Release latestRelease = await _gitHubClient.Repository.Release.GetLatest(repository.Id);
-                    apps.Add(CreateApp(repository, latestRelease));
-                }
-                catch (NotFoundException)
-                {
+                    IGitHubRelease latestRelease = await _releaseFinderService.GetLatestRelease(repository);
+
+                    App repositoryApp = _repositoryToAppGeneratorService.GenerateApp(new GitHubRepositoryDataProvider(repository, latestRelease));
+
+                    apps[repositoryIndex] = repositoryApp;
                 }
             }
 
             return apps;
-        }
-
-        private static App CreateApp(Repository repository, Release latestRelease)
-        {
-            return new App
-            {
-                Description = GetAppDescription(latestRelease),
-                DownloadUrl = GetAppDownloadUrl(latestRelease),
-                GitHubUrl = GetGitHubUrl(repository),
-                LatestVersion = GetAppVersion(latestRelease),
-                LastUpdated = GetAppReleaseDate(latestRelease),
-                Name = GetAppName(repository)
-            };
-        }
-
-        private static string GetAppDescription(Release latestRelease)
-        {
-            string bodyText = latestRelease.Body;
-
-            string[] bodyTextParagraphs = bodyText.Split("\r\n\r\n");
-            string firstParagraph = bodyTextParagraphs[0];
-
-            string[] firstParagraphLines = firstParagraph.Split("\r\n");
-            string firstParagraphBody = string.Join("\r\n", firstParagraphLines.Skip(1));
-
-            return firstParagraphBody;
-        }
-
-        private static string GetAppDownloadUrl(Release latestRelease)
-        {
-            return latestRelease.Assets.First(asset => asset.Name == "Setup.exe" || asset.Name == "Release.zip").BrowserDownloadUrl;
-        }
-
-        private static string GetGitHubUrl(Repository repository)
-        {
-            return repository.HtmlUrl;
-        }
-
-        private static string GetAppVersion(Release latestRelease)
-        {
-            return latestRelease.TagName;
-        }
-
-        private static DateTime GetAppReleaseDate(Release latestRelease)
-        {
-            return latestRelease.PublishedAt.Value.UtcDateTime;
-        }
-
-        private static string GetAppName(Repository repository)
-        {
-            return repository.Name.Replace('-', ' ');
         }
     }
 }
